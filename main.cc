68732f437aa947c79e36921d49fbfc7e5d638e39
Hasan <71188244+Cuersy@users.noreply.github.com>
2025-04-01 21:51:19 +0300

    Update main.cc
    bpm ozelligi

--- main.cc
+++ main.cc
@@ -3,99 +3,103 @@
 #include <string>
 #include <vector>
 #include <windows.h>
+#include <algorithm>
 
-BOOL CALLBACK enum_windows_proc(HWND hwnd, LPARAM lparam) {
-    char win_title[256];
-    GetWindowTextA(hwnd, win_title, sizeof(win_title));
-    if (strstr(win_title, "Roblox") != nullptr) {
+using namespace std;
+
+BOOL CALLBACK screenhndlr(HWND hwnd, LPARAM lParam) {
+    char windowTitle[256];
+    GetWindowTextA(hwnd, windowTitle, sizeof(windowTitle));
+    if (strstr(windowTitle, "Roblox") != nullptr) {
         SetActiveWindow(hwnd);
         SetForegroundWindow(hwnd);
-        std::cout << "focused to roblox window!" << std::endl;
+        std::cout << "Focused to roblox window!" << std::endl;
         return FALSE;
     }
     return TRUE;
 }
 
-void set_roblox_focus() {
-    EnumWindows(enum_windows_proc, 0);
+void setroblox() {
+    EnumWindows(screenhndlr, 0);
 }
 
-void key_action(char key_param, bool press) {
-    SHORT key = VkKeyScan(key_param);
-    UINT mapped_key = MapVirtualKey(LOBYTE(key), MAPVK_VK_TO_VSC);
+void press(char keyParam) {
+    SHORT key = VkKeyScan(keyParam);
+    UINT mapped = MapVirtualKey(LOBYTE(key), MAPVK_VK_TO_VSC);
     INPUT input = { 0 };
     input.type = INPUT_KEYBOARD;
-    input.ki.dwFlags = press ? KEYEVENTF_SCANCODE : (KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP);
-    input.ki.wScan = mapped_key;
+    input.ki.dwFlags = KEYEVENTF_SCANCODE;
+    input.ki.wScan = mapped;
     SendInput(1, &input, sizeof(INPUT));
 }
 
-bool is_paused = false;
-void toggle_pause() {
-    is_paused = !is_paused;
-    std::cout << (is_paused ? "basma lan." : "basom lan.") << std::endl;
-}
-
-void process_key_sequence(const std::string& sequence, int interval) {
-    for (size_t i = 0; i < sequence.size(); i++) {
-        if (sequence[i] == '[') {
-            std::vector<char> pressed_keys;
-            i++;
-            while (i < sequence.size() && sequence[i] != ']') {
-                if (sequence[i] != ' ') pressed_keys.push_back(sequence[i]);
-                i++;
-            }
-            for (char c : pressed_keys) key_action(c, true);
-            Sleep(interval / 2);
-            for (char c : pressed_keys) key_action(c, false);
-        }
-        else if (sequence[i] == '|') {
-            int pause_duration = interval;
-            while (i + 1 < sequence.size() && sequence[i + 1] == ' ') {
-                pause_duration += interval / 2;
-                i++;
-            }
-            Sleep(pause_duration);
-        }
-        else if (sequence[i] != ' ') {
-            key_action(sequence[i], true);
-            Sleep(interval / 5);
-            key_action(sequence[i], false);
-        }
-    }
+void release(char keyParam) {
+    SHORT key = VkKeyScan(keyParam);
+    UINT mapped = MapVirtualKey(LOBYTE(key), MAPVK_VK_TO_VSC);
+    INPUT input = { 0 };
+    input.type = INPUT_KEYBOARD;
+    input.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+    input.ki.wScan = mapped;
+    SendInput(1, &input, sizeof(INPUT));
 }
 
 int main() {
-    int bpm;
-    std::cout << "Enter BPM(if u don't know just search on google) : ";
-    std::cin >> bpm;
-    if (bpm <= 0) {
-        std::cerr << "bpm value must be greater than 0" << std::endl;
-        while (1) {}
-    }
-    
-    set_roblox_focus();
-    int interval = 60000 / bpm;
+    setroblox();
 
-    std::ifstream file("keys.txt");
+    ifstream file("keys.txt");
     if (!file.is_open()) {
-        std::cerr << "i cant find keys.txt" << std::endl;
-        while (1) {}
+        cerr << "i cant find keys.txt" << endl;
+        return 1;
     }
 
-    std::string line;
-    while (std::getline(file, line)) {
-        if (GetAsyncKeyState(0x50) & 0x8000) {
-            toggle_pause();
-            Sleep(100);
-        }
-        if (is_paused) {
-            Sleep(50);
-            continue;
-        }
-        process_key_sequence(line, interval);
-        Sleep(interval);
-    }
+    string allkeys((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
     file.close();
+    allkeys.erase(remove(allkeys.begin(), allkeys.end(), '\n'), allkeys.end());
+    allkeys.erase(remove(allkeys.begin(), allkeys.end(), '\r'), allkeys.end());
+
+    int interval = 0;
+    size_t i = 0;
+
+    while (i < allkeys.size()) {
+        if (allkeys.substr(i, 5) == "<bpm>") {
+            size_t end_bpm = allkeys.find("</bpm>", i);
+            if (end_bpm != string::npos) {
+                string bpmstr = allkeys.substr(i + 5, end_bpm - (i + 5));
+                int newbpm = stoi(bpmstr);
+                interval = 60000 / newbpm;
+                i = end_bpm + 6;
+                continue;
+            }
+        }
+
+        if (interval > 0) {
+            if (allkeys[i] == '[') {
+                vector<char> pressedvector;
+                i++;
+                while (i < allkeys.size() && allkeys[i] != ']') {
+                    if (allkeys[i] != ' ') pressedvector.push_back(allkeys[i]);
+                    i++;
+                }
+                for (char c : pressedvector) press(c);
+                Sleep(interval / 2.5);
+                for (char c : pressedvector) release(c);
+            }
+            else if (allkeys[i] == '|') {
+                int pauseDuration = interval;
+                while (i + 1 < allkeys.size() && allkeys[i + 1] == ' ') {
+                    pauseDuration += interval / 2;
+                    i++;
+                }
+                Sleep(pauseDuration);
+            }
+            else if (allkeys[i] != ' ') {
+                press(allkeys[i]);
+                Sleep(interval / 5);
+                release(allkeys[i]);
+            }
+        }
+        i++;
+    }
+
     return 0;
 }
